a60f45e0a97d0c25c416e0ac374881d815f110e0
Shane Ursani <shaneursani@gmail.com>
2025-05-20 21:28:34 -0500

    fix(frontend): improve gallery grid

--- frontend/src/components/GalleryView.tsx
+++ frontend/src/components/GalleryView.tsx
@@ -70,7 +70,7 @@ const GalleryView: React.FC = () => {
                 </div>
             )}
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-md gap-y-lg mb-xl">
+            <div className="gallery-grid mb-xl">
                 {images.map(metadata => (
                     <ImageCard key={metadata.id} metadata={metadata} />
                 ))}
